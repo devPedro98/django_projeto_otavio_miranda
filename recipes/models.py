13c89de61bdba4a1f4e82bb092ded127d593fa68
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-26 18:59:37 -0300

    self.client to test

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -24,8 +22,8 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')  # noqa
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)  # noqa
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
